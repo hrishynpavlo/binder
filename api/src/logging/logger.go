b76953caf81e5ed149ce358164ab5aa9e1003a18
pavlo.hrishyn <pashagrishyn@gmai.com>
2023-08-13 22:23:18 +0200

    removed file logging

--- api/src/logging/logger.go
+++ api/src/logging/logger.go
@@ -1,9 +1,7 @@
 package logging
 
 import (
-	"fmt"
 	"os"
-	"time"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -13,12 +11,12 @@ func ProviderLogger() *zap.Logger {
 
 	encoder := zapcore.NewJSONEncoder(zap.NewDevelopmentEncoderConfig())
 
-	file, _ := os.Create(fmt.Sprintf("binder_api_%d.log", time.Now().Unix()))
+	//file, _ := os.Create(fmt.Sprintf("binder_api_%d.log", time.Now().Unix()))
 
-	fileLogger := zapcore.NewCore(encoder, zapcore.AddSync(file), zapcore.DebugLevel)
+	//fileLogger := zapcore.NewCore(encoder, zapcore.AddSync(file), zapcore.DebugLevel)
 	consoleLogger := zapcore.NewCore(encoder, zapcore.AddSync(os.Stdout), zapcore.DebugLevel)
 
-	core := zapcore.NewTee(consoleLogger, fileLogger)
+	core := zapcore.NewTee(consoleLogger)
 	logger := zap.New(core, zap.AddCaller())
 
 	return logger
